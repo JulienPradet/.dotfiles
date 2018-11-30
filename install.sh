97393f34e81c0256b1e0adad1a769556c2374954
Julien Pradet <julien.pradet@gmail.com>
2018-11-30 20:53:24 +0100

    Update script after first install on a second pc

--- install.sh
+++ install.sh
@@ -3,29 +3,33 @@
 sudo echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
 rmmod pcspkr
 
-pamac install google-chrome thunderbird code slack-desktop docker docker-compose unbound
-pamac build google-chrome
-pamac build slack-desktop
-pamac build spotify
-pamac build gitflow-avh
+pamac update
+pamac install thunderbird code docker docker-compose unbound ansible tk
+pamac build google-chrome slack-desktop spotify gitflow-avh
 pamac remove dnsmasq
 
 sudo systemctl start docker
 sudo systemctl enable docker
-# sudo usermod -aG docker $(whoami)
+sudo usermod -aG docker $(whoami)
 
-systemctl disable dnsmasq
-systemctl stop dnsmasq
+sudo systemctl disable dnsmasq
+sudo systemctl stop dnsmasq
 sudo cp ~/.dotfiles/unbound.conf /etc/unbound/unbound.conf
+sudo systemctl enable unbound
+sudo systemctl start unbound
+# Configure network Ipv4settings => Automatic (DHCP) addresses only
 
 docker network create --subnet=172.10.0.0/16 traefik
 docker run -d --restart always --name traefik --net traefik --ip 172.10.0.10 -v /var/run/docker.sock:/var/run/docker.sock:ro traefik --web --docker --docker.domain=test --loglevel=INFO
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-ln -s -T ~/.dotfiles/.zshrc ~/.zshrc
+cp ~/.dotfiles/.zshrc ~/.zshrc
 source ~/.zshrc
 
 timedatectl set-ntp true 
 timedatectl set-timezome Europe/Paris
 
-ln -s -T ~/.dotfiles/.Xresources ~/.Xresources
+cp ~/.dotfiles/.Xresources ~/.Xresources
+xrdb ~/.Xresources
+
+cp ~/.dotfiles/i3config ~/.i3/config
