0a42ec09d91c4b5ec005d9d2ea366bfef4723f93
Julien Pradet <julien.pradet@singulart.com>
2021-04-12 10:56:01 +0200

    Update with latest config

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ sudo echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
 rmmod pcspkr
 
 pamac update
-pamac install thunderbird code docker docker-compose unbound ansible tk pavucontrol pulseaudio-bluetooth
+pamac install z code docker docker-compose unbound tk pavucontrol pulseaudio-bluetooth jdk-openjdk opendesktop-fonts ttf-baekmuk
 killall pulseaudio
-pamac build google-chrome slack-desktop spotify gitflow-avh
+pamac build google-chrome slack-desktop spotify gitflow-avh phpstorm
 pamac remove dnsmasq
 
 sudo systemctl start docker
@@ -24,6 +24,7 @@ docker network create --subnet=172.10.0.0/16 traefik
 docker run -d --restart always --name traefik --net traefik --ip 172.10.0.10 -v /var/run/docker.sock:/var/run/docker.sock:ro traefik --web --docker --docker.domain=test --loglevel=INFO
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
 cp ~/.dotfiles/.zshrc ~/.zshrc
 
 timedatectl set-ntp true 
